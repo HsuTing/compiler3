81fdb9bf278559b088ddad41d0637d2205173a29
HsuTing <hsuting0106@gmail.com>
2015-06-18 01:14:49 +0800

    add test file

---
+++ test.c
@@ -0,0 +1,22 @@
+int x;
+
+int plus(int a, int b) {
+	int c;
+	c = a + b;
+	return c;
+}
+
+int main() {
+	int y[2];
+	y[0] = 3;
+	y[1] = 4;
+	x = plus(y[0], y[1]);
+	if(x) {
+		double a;
+	}
+	else {
+		float b;
+	}
+
+	print x;
+}
