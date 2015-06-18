ab7fe1d3879a336c3847e516eb135ea07801b441
HsuTing <hsuting0106@gmail.com>
2015-06-18 17:26:14 +0800

    add mina.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include "symbol/symbol.h"
+#include "check/check.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	if(argc == 2) {
+		ifstream ft(argv[1]);
+
+		if(ft != NULL) {
+			SymbolTable symbol = SymbolTable(argv[1]);
+		}
+		else {
+			cout << "Can not find \"" << argv[1] << "\"." << endl;
+		}
+
+		ft.close();
+	}
+	else if(argc == 1) {
+		cout << "Please input file name." << endl;
+	}
+	else {
+		cout << "Input too much argument." << endl;
+	}
+
+	return 0;
+}
