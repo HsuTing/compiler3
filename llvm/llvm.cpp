61207bafe4990f19cb8af031ec032f9231ab5735
HsuTing <hsuting0106@gmail.com>
2015-06-20 21:07:29 +0800

    add llvm

--- llvm/llvm.cpp
+++ llvm/llvm.cpp
@@ -1,4 +1,18 @@
 #include "llvm.h"
 
 Llvm::Llvm() {
+	ifstream fin("data/tree.txt");
+
+	string line = "";
+	while(getline(fin, line)) {
+		istringstream fin_word(line);
+		string word = "";
+
+		while(fin_word >> word) {
+			cout << word << " ";
+		}
+		cout << endl;
+	}
+
+	fin.close();
 }
